3c0a5bc600860762a5aca19b540592d183ebfc57
Joey Palchak <joey.palchak@gmail.com>
2023-12-14 20:22:17 -0800

    add migrations to reflect User DateTime field

--- CranberryAPI/Migrations/20231215042151_ChangeUserDateBackToDateTime.Designer.cs
+++ CranberryAPI/Migrations/20231215042151_ChangeUserDateBackToDateTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CranberryAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CranberryAPI.Migrations
 {
     [DbContext(typeof(CranberryAPIContext))]
-    partial class CranberryAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20231215042151_ChangeUserDateBackToDateTime")]
+    partial class ChangeUserDateBackToDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CranberryAPI/Migrations/20231215042151_ChangeUserDateBackToDateTime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CranberryAPI.Migrations
+{
+    public partial class ChangeUserDateBackToDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "QuitDate",
+                table: "AspNetUsers",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuitDate",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
