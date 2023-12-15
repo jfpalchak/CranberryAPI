013257b2d2da9cd990bfeee391cd87aadc24056e
Joey Palchak <joey.palchak@gmail.com>
2023-12-15 08:02:50 -0800

    update migrations to reflect Date type changes, v2

--- CranberryAPI/Models/UserProfileDto.cs
+++ CranberryAPI/Models/UserProfileDto.cs
@@ -5,7 +5,7 @@ public class UserProfileDto
 {
   public string UserId { get; set; }
   public string UserName { get; set; }
-  public DateTime QuitDate { get; set; }
+  public string QuitDate { get; set; }
   public int AvgSmokedDaily { get; set; }
   public int PricePerPack { get; set; }
   public int CigsPerPack { get; set; }
